4d72dd48727a2859fb4c92f76669c6cc3f08822f
Emine Kurucu <emine.kurucu.7@gmail.com>
2024-05-22 12:03:09 +0300

    Create uygulama_girisi.py
    Bu programda kullanıcıdan mail ve parola istiyoruz. Eğer girilen parola ve mail sisteme kayıtlı olanlarla eşleşirse kullanıcı girişi başarılı olur. Eğer ikisinden biri eşleşmezse hata mesajı oynatılır

---
+++ uygulama_girisi.py
@@ -0,0 +1,30 @@
+def main():
+    email = input("Email : ")
+    parola = input("Parola: ")
+    
+    if isMail_correct(email):
+        if isParola_correct(parola):
+            print("Giris basarili")
+        else:
+            print("Parolaniz yanlis.")
+            print("Tekrar deneyiniz.")
+    else :
+        print("Email adresiniz yanlis.")
+        print("Tekrar deneyiniz.")
+        
+def isMail_correct(kullanici_maili):
+    dogru_mail  = "email@deneme.com"
+    if (kullanici_maili == dogru_mail):
+        return True
+    else:
+        return False
+    
+def isParola_correct(kullanici_parolasi):
+    dogru_parola = "abc007"
+    if (kullanici_parolasi == dogru_parola):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
